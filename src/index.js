5f6db2940565263297ed65601fa07d2a503e9a45
Geekie <xavi.contact@gmail.com>
2018-03-05 00:59:25 +0000

    babel fixed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('debug');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Paulo Vieira ReactJS Boilerplate';
+const app = document.getElementById('app');
+var content = (
+    <h1>{title}</h1>
+);
+
+ReactDOM.render(content, app);
+
+module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,32 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: [
+    'react-hot-loader/patch',
     './src/index.js',
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_molues/,
-        use: ['babel_loader']
+        exclude: /node_modules/,
+        use: ['babel-loader'],
       }
     ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx'],
   },
   output: {
-    path: `${__dirname }/dist`,
+    path: `${__dirname}/dist`,
     publicPath: '/',
     filename: 'bundle.js',
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   devServer: {
     contentBase: './dist',
+    hot: true,
   },
 };
